27db07adbfa988599ebeb557ffbf1fcb9b9e3cc4
Victor <arreagav17@gmail.com>
2023-07-17 13:53:00 -0500

    Deleted

--- lib/models/disease_tomato_leaf.dart
+++
@@ -1,7 +0,0 @@
-class DiseaseTomatoLeaf {
-  final String label;
-  final double confidence;
-  const DiseaseTomatoLeaf(this.label, this.confidence);
-
-  get confidencePercent => (confidence * 100).toStringAsFixed(2);
-}
